90b909d183aa281f9c02b8c956b3d2a0cefbc31a
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-05-16 07:17:05 -0700

    initial setup

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import express from 'express';
-
-require('./db/mongoose');
-require('dotenv').config();
-
 import morgan from 'morgan';
+import './db/mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 // Middleware
 app.use(morgan('dev'));
 
-app.listen(PORT, () => {
-    console.log(`서버가 시작했습니다. http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
