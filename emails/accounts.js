e87d524ce6c22f31b1a9fecf7def2dafa428161a
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-06-14 05:17:00 -0700

    employee signup route

--- emails/accounts.js
+++ emails/accounts.js
@@ -7,7 +7,7 @@ const sendInvitationEmail = (name, email, locationId) => {
     to: email,
     from: 'dongwan.don.kim@gmail.com',
     subject: '알바로그: 직원초대',
-    text: `안녕하세요, ${name}님. 아래 링크를 눌러 회원가입을 완료하세요 http://localhost:5000/api/v1/employee/${locationId}/signup`,
+    text: `안녕하세요, ${name}님. 아래 링크를 눌러 회원가입을 완료하세요 http://localhost:3000/parttime/${locationId}/signup`,
   });
 };
 
