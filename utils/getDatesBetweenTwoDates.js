538c6e02fb52e4547f496938a1be712f31ffc55f
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-06-26 20:45:37 -0700

    js dates to momentjs

--- utils/getDatesBetweenTwoDates.js
+++ utils/getDatesBetweenTwoDates.js
@@ -10,7 +10,7 @@ async function dateRange(
   time
 ) {
   const dateArray = [];
-  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   let currentDate = moment.utc(startDate).toDate();
 
   // const shift = await Shift.find({ owner: staffId, location: locationId });
@@ -20,15 +20,15 @@ async function dateRange(
 
   while (currentDate <= moment.utc(endDate).toDate()) {
     time.forEach((d) => {
-      if (d.day !== currentDate.getDay()) return;
+      if (d.day !== moment.utc(currentDate).day()) return;
 
       let day = days.filter((day) => days.indexOf(day) === d.day);
 
       //utc 0 db
       //server utc 0
 
-      const start_time = moment.utc(currentDate).toDate();
-      const end_time = moment.utc(currentDate).toDate();
+      let start_time = moment.utc(currentDate).toDate();
+      let end_time = moment.utc(currentDate).toDate();
 
       // const start_time = new Date(currentDate);
       // const end_time = new Date(currentDate);
