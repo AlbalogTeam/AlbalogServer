dfa57d31ff9e16263d6b4e462fbcbb0850041cda
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-06-26 20:09:56 -0700

    timezone fix

--- utils/getDatesBetweenTwoDates.js
+++ utils/getDatesBetweenTwoDates.js
@@ -27,25 +27,14 @@ async function dateRange(
       //utc 0 db
       //server utc 0
 
-      //   {
-      //     "_id": "60d71987fa83e02c69cfd1ce",
-      //     "date": "2021-05-29T00:00:00.000Z",
-      //     "day": "Sat",
-      //     "start": "2021-05-29T09:00:00.000Z",
-      //     "end": "2021-05-29T15:00:00.000Z",
-      //     "owner": "60d17fcdd500db0f93a70f77",
-      //     "location": "60c856a7a941aa698d77105c",
-      //     "__v": 0
-      // },
-
-      const start_time = moment.utc(currentDate).toDate();
-      const end_time = moment.utc(currentDate).toDate();
+      const start_time = moment.utc(currentDate).offset(0, true).format();
+      const end_time = moment.utc(currentDate).offset(0, true).format();
       // const start_time = new Date(currentDate);
       // const end_time = new Date(currentDate);
       const st = d.start_time.split(':');
       const et = d.end_time.split(':');
-      start_time.setHours(st[0], st[1]);
-      end_time.setHours(et[0], et[1]);
+      start_time.setUTCHours(st[0], st[1]);
+      end_time.setUTCHours(et[0], et[1]);
 
       dateArray.push({
         date: moment.utc(currentDate).toDate(),
