da2a16491fe752e930ec216f96d300d197b7c91c
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-07-13 02:53:57 -0700

    create shift: startTime & endTime issue fixed

--- models/location/location.js
+++ models/location/location.js
@@ -146,7 +146,7 @@ const locationSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-const Location = mongoose.model('Location', locationSchema);
+
 // 스태프가 해당매장의 스태프인지 확인
 locationSchema.statics.checkIfUserBelongsToLocation = async (
   locationId,
@@ -175,5 +175,6 @@ locationSchema.statics.isValidCreateShift = async (
   if (!isValid) return false;
   return true;
 };
+const Location = mongoose.model('Location', locationSchema);
 
 module.exports = Location;
